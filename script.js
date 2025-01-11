f330f0c205b67129c44442684f5fb6659287281c
Your Name <you@example.com>
2025-01-11 12:33:01 +0100

    CRUD(Array)

--- script.js
+++ script.js
@@ -53,12 +53,49 @@ function render(){
     kontener.appendChild(sor)
     // Fejléc készítés vége
 
+    // Új adat felvitele
+    {
+
+        const sor= divFactory("","row my-2")
+        for (const oszlop of stage.oszlopok) {
+            // sor.appendChild(divFactory(animal[oszlop],"col"))
+            const cella= divFactory("","col")
+            const beMezo= document.createElement("input")
+            beMezo.className="form-control"
+          
+            beMezo.id="new"+oszlop
+            cella.appendChild(beMezo)
+            sor.appendChild(cella)
+
+        }
+        const cella=divFactory("","col")
+
+        const addBtn = document.createElement("button")
+        addBtn.className="btn btn-primary"
+        addBtn.innerHTML="Hozzáad"
+        addBtn.addEventListener("click", addAnimal)
+        cella.appendChild(addBtn)
+
+        sor.appendChild(cella)
+        kontener.appendChild(sor)
+    }
+    
+    // Új adat vége
+
     // Adatok kiíratása
     for (const animal of stage.animals) {  
 
         const sor= divFactory("","row my-2")
         for (const oszlop of stage.oszlopok) {
-            sor.appendChild(divFactory(animal[oszlop],"col"))
+            // sor.appendChild(divFactory(animal[oszlop],"col"))
+            const cella= divFactory("","col")
+            const beMezo= document.createElement("input")
+            beMezo.className="form-control"
+            beMezo.value=animal[oszlop]
+            beMezo.id=animal.id+oszlop
+            cella.appendChild(beMezo)
+            sor.appendChild(cella)
+
         }
         const cella=divFactory("","col")
 
@@ -66,6 +103,7 @@ function render(){
         editBtn.className="btn btn-primary"
         editBtn.innerHTML="Szerkeszt"
         editBtn.addEventListener("click", updateAnimal)
+        editBtn.dataset.id=animal.id
         cella.appendChild(editBtn)
 
         const delBtn = document.createElement("button")
@@ -79,12 +117,39 @@ function render(){
         kontener.appendChild(sor)
 
     }
-    // Adat kiíratás vége
+        // Adat kiíratás vége
+}
+
+function addAnimal(){
+    console.log("add")
+    const newAnimal={}
+
+    for (const oszlop of stage.oszlopok) {
+        const ertek= document.getElementById("new"+oszlop).value
+        console.log(ertek)
+        newAnimal[oszlop]=ertek
+    }
+    const max= Math.max(...stage.animals.map((allat)=>Number(allat.id)))
+    newAnimal.id=max+1
+    stage.animals.push(newAnimal)
+    render()
+    
+    console.log("Maximum:",max)
+    console.log(stage.animals)
 }
 
 function updateAnimal(){
-    console.log("update")
+    console.log("update",this.dataset.id)
+
+    for (const oszlop of stage.oszlopok) {
+        const ertek= document.getElementById(this.dataset.id+oszlop).value
+        console.log(ertek)
+        stage.animals[Number(this.dataset.id)-1][oszlop]=ertek
+    }
+    console.log(stage.animals)
 }
+
+//CRUD C-Create, R-Read, U-Update, D-Delete
 
 function deleteAnimal(){
     console.log("delete", this.dataset.id)
